caa839eff11cc98ba9674569e8ffde9bdfb19e2b
Codcu-entel <88192139+Codcu-entel@users.noreply.github.com>
2023-11-08 22:36:23 +0300

    Admin ve UI Altyapısının Geliştirilmesi, Angular Multiple Layout Altyapı İnşası, Angular Material Altyapı Entegrasyonu, Angular Bootstrap Jquery Entegrasyonu

---
+++ src/app/admin/components/customers/customers.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CustomersComponent } from '../customers/customers.component';
+import { RouterModule } from '@angular/router';
+
+
+
+@NgModule({
+  declarations: [
+    CustomersComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule.forChild([
+      {path: "", component: CustomersComponent}
+    ])
+  ]
+})
+export class CustomersModule { }
