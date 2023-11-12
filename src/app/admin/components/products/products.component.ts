2df9054c5535e5eb88e870c9042413fdc46985dc
Codcu-entel <88192139+Codcu-entel@users.noreply.github.com>
2023-11-12 19:22:26 +0300

    Ürün Ekleme İşlemleri - ProductServcice

--- src/app/admin/components/products/products.component.ts
+++ src/app/admin/components/products/products.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
-import { Product } from 'src/app/contracts/product';
 import { HttpClientService } from 'src/app/services/common/http-client.service';
 
 @Component({
@@ -18,34 +17,6 @@ export class ProductsComponent extends BaseComponent implements OnInit {
   ngOnInit(): void {
     this.showSpinner(SpinnerType.BallAtom);
 
-    this.httpClientService.get<Product[]>({
-      controller: "products"
-    }).subscribe(data => console.log(data));
 
-    // this.httpClientService.post({
-    //   controller: "products"
-    // }, {
-    //   name: "Kalem",
-    //   stock: 100,
-    //   price: 15
-    // }).subscribe();
-
-    // this.httpClientService.put({
-    //   controller: "products"
-    // }, {
-    //   id: "6f665a38-1920-4467-8652-048ca977f112",
-    //   name: "Renkli Kağit",
-    //   stock: 1500,
-    //   price: 5.5
-    // }).subscribe();
-
-    // this.httpClientService.delete({
-    //   controller: "products"
-    // }, "ed6d8b5b-3681-4950-bc2e-26fd581369bb")
-    // .subscribe();
-
-    // this.httpClientService.get({
-    //   fullEndPoint: "https://jsonplaceholder.typicode.com/posts",
-    // }).subscribe(data => console.log(data));
   }
 }
