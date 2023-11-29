f7453c5e55ba714aa5c476748856d061dde23bc8
Codcu-entel <88192139+Codcu-entel@users.noreply.github.com>
2023-11-29 22:33:37 +0300

    Delete Modal Dialog İle Silme Onaylama

--- src/app/directives/admin/delete.directive.ts
+++ src/app/directives/admin/delete.directive.ts
@@ -1,6 +1,8 @@
 import { Directive, ElementRef, EventEmitter, HostListener, Input, Output, Renderer2 } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { NgxSpinner, NgxSpinnerService } from 'ngx-spinner';
 import { SpinnerType } from 'src/app/base/base.component';
+import { DeleteDialogComponent, DeleteState } from 'src/app/dialogs/delete-dialog/delete-dialog.component';
 import { ProductService } from 'src/app/services/common/models/product.service';
 
 declare var $: any;
@@ -14,7 +16,8 @@ export class DeleteDirective {
     private element: ElementRef,
     private _renderer: Renderer2,
     private productService: ProductService,
-    private spinner: NgxSpinnerService
+    private spinner: NgxSpinnerService,
+    public dialog: MatDialog
   ) {
     const img = _renderer.createElement("img");
     img.setAttribute("src", "../../../../../assets/delete.png");
@@ -29,11 +32,30 @@ export class DeleteDirective {
 
   @HostListener("click")
   async onclick() {
-    this.spinner.show(SpinnerType.BallAtom);
+    this.openDialog(async ()=>{
+      this.spinner.show(SpinnerType.BallAtom);
      const td: HTMLTableCellElement = this.element.nativeElement;
      await this.productService.delete(this.id);
-     $(td.parentElement).fadeOut(2000, ()=> {
+     $(td.parentElement).animate({
+      opacity: 0,
+      left: "+=50",
+      height: "toogle"
+     }, 700, ()=> {
       this.callback.emit();
      });
+    });
+  }
+
+  openDialog(afterClosed: any): void {
+    const dialogRef = this.dialog.open(DeleteDialogComponent, {
+      width: '250px',
+      data: DeleteState.Yes,
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+     if(result == DeleteState.Yes){
+      afterClosed();
+     }
+    });
   }
 }
