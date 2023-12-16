1ced717ff970fbeb6a01623683da31bcb439e76a
Codcu-entel <88192139+Codcu-entel@users.noreply.github.com>
2023-12-16 19:55:24 +0300

    Guard Yapılanması İle Yetkisiz Erişim Engellemesi

---
+++ src/app/guards/common/auth.guard.ts
@@ -0,0 +1,44 @@
+import { inject } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivateFn, Router, RouterStateSnapshot } from '@angular/router';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { SpinnerType } from 'src/app/base/base.component';
+import { CustomToastrService, ToastrMessageType, ToastrPosition } from 'src/app/services/ui/custom-toastr.service';
+
+export const AuthGuard: CanActivateFn = (route: ActivatedRouteSnapshot, state: RouterStateSnapshot,) => {
+  const jwtHelper = inject(JwtHelperService);
+  const router = inject(Router);
+  const toastrService = inject(CustomToastrService);
+  const spinner = inject(NgxSpinnerService);
+
+  spinner.show(SpinnerType.BallAtom);
+  const token: string = localStorage.getItem("accessToken");
+
+  //const decodeToken = this.jwtHelper.decodeToken(token);
+  //const expirationDate: Date = this.jwtHelper.getTokenExpirationDate(token);
+  let expired: boolean;
+  try {
+    expired = jwtHelper.isTokenExpired(token);
+  } catch {
+    expired = true;
+  }
+
+  if (!token || expired) {
+    router.navigate(["login"], { queryParams: { returnUrl: state.url } });
+    toastrService.message("Oturum açmanız gerekiyor!", "Yetkisiz Erişim!", {
+      messageType: ToastrMessageType.Warning,
+      position: ToastrPosition.TopRight
+    })
+  }
+
+
+  spinner.hide(SpinnerType.BallAtom);
+
+  return true;
+};
+
+
+
+
+
+
