784324b43b18baabd62b806899449b7957b67849
Codcu-entel <88192139+Codcu-entel@users.noreply.github.com>
2023-12-16 21:18:37 +0300

    IdentityCheck İle Link Kontrolü-ReturnURL İşlevi

--- src/app/guards/common/auth.guard.ts
+++ src/app/guards/common/auth.guard.ts
@@ -3,6 +3,7 @@ import { ActivatedRouteSnapshot, CanActivateFn, Router, RouterStateSnapshot } fr
 import { JwtHelperService } from '@auth0/angular-jwt';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { SpinnerType } from 'src/app/base/base.component';
+import { _isAuthenticated } from 'src/app/services/common/auth.service';
 import { CustomToastrService, ToastrMessageType, ToastrPosition } from 'src/app/services/ui/custom-toastr.service';
 
 export const AuthGuard: CanActivateFn = (route: ActivatedRouteSnapshot, state: RouterStateSnapshot,) => {
@@ -12,18 +13,18 @@ export const AuthGuard: CanActivateFn = (route: ActivatedRouteSnapshot, state: R
   const spinner = inject(NgxSpinnerService);
 
   spinner.show(SpinnerType.BallAtom);
-  const token: string = localStorage.getItem("accessToken");
+  //const token: string = localStorage.getItem("accessToken");
 
-  //const decodeToken = this.jwtHelper.decodeToken(token);
-  //const expirationDate: Date = this.jwtHelper.getTokenExpirationDate(token);
-  let expired: boolean;
-  try {
-    expired = jwtHelper.isTokenExpired(token);
-  } catch {
-    expired = true;
-  }
+    //const decodeToken = this.jwtHelper.decodeToken(token);
+    //const expirationDate: Date = this.jwtHelper.getTokenExpirationDate(token);
+    //let expired: boolean;
+    //try {
+    //  expired = this.jwtHelper.isTokenExpired(token);
+    //} catch {
+    //  expired = true;
+    //}
 
-  if (!token || expired) {
+    if (!_isAuthenticated) {
     router.navigate(["login"], { queryParams: { returnUrl: state.url } });
     toastrService.message("Oturum açmanız gerekiyor!", "Yetkisiz Erişim!", {
       messageType: ToastrMessageType.Warning,
